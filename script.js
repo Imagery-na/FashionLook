3050900640f44d9ca2ac813ad308a4c47a29c7eb
SportProject <38287103+Imagery-na@users.noreply.github.com>
2019-07-05 19:35:52 +1000

    цена

--- script.js
+++ script.js
@@ -6,6 +6,20 @@ $(function()
      $(this).toggleClass('open');
      $(this).next('.list_link').toggleClass('open');
   });
+  $('#slider_price').slider({
+    max: 1000, 
+    min: 0,
+    range: true,
+    values: [190, 728],
+    slide: function( event, ui) {
+      $('input[name="minPrice"]').val( '$'+ ui.values[0]);
+      $('input[name="maxPrice"]').val( '$'+ ui.values[1]);
+    }
+  });
 
+  $('input[name="minPrice"]').val( 
+    $('#slider_price').slider('values', 0) );
+  $('input[name="maxPrice"]').val( 
+      $('#slider_price').slider('values', 1) );
 
 });
