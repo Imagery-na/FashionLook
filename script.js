91ebea313f7cfcc85149d78c87f49f5ba7974b46
SportProject <38287103+Imagery-na@users.noreply.github.com>
2019-07-09 23:36:52 +1000

    Готово!

--- script.js
+++ script.js
@@ -33,5 +33,12 @@ $(function()
     $('#black_fill').toggleClass('open');
     $('#modal').toggleClass('open');
   });
+  $('#search_button').click(function(){
+    $('#search_panel').toggleClass('open');
+  });
+  $('#loadmore').click(function(event){
+    event.preventDefault();
+    $('.list_product').append('<a class="product box anime flex_main flex_nowrap flex_jcontent-between flex_aitem-center"><div class="img_product"><img src="img/clothes2.png" alt=""></div><h2>Худи</h2><p>Fashion-professional</p><p class="price">$270</p></a><a class="product box anime flex_main flex_nowrap flex_jcontent-between flex_aitem-center"><div class="tag box">NEW</div><div class="img_product"><img src="img/clothes4.png" alt=""></div><h2>Кофта</h2><p>Burberries</p><p class="price">$180</p></a><a class="product box anime flex_main flex_nowrap flex_jcontent-between flex_aitem-center"><div class="img_product"><img src="img/clothes6.png" alt=""></div><h2>Бомбер</h2><p>Be yourself</p><p class="price">$290</p></a>')
+  });
 
 });
