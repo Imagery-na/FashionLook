6b1c84c9c45b1801d8405fd5e45cf170f03e273b
SportProject <38287103+Imagery-na@users.noreply.github.com>
2019-11-05 16:38:51 +1000

    убрала лишние стили по умолчанию

--- script.js
+++ script.js
@@ -1,12 +1,64 @@
+'use strict';
+//работа с корзиной
+const products = document.querySelectorAll('.list_product .product');
+const contentCart = document.querySelector('.content_cart');
+products.forEach((product) => {
+  const btn = product.querySelector('button');
+  btn.addEventListener('click', () => {
+    const productClone = product.cloneNode(true);
+    contentCart.appendChild(productClone);
+    showData();
+    productClone.style.height = '';
+    const removeBtn = productClone.querySelector('button');
+    removeBtn.textContent = 'Удалить из корзины';
+    removeBtn.addEventListener('click', () => {
+      productClone.remove();
+      showData();
+    });
+  });
+
+  function showData() {
+    const productsCart = contentCart.querySelectorAll('.product'),
+        productsPrice = contentCart.querySelectorAll('.product .price'), // все цены
+        priceTotal = document.querySelector('.price_total'), // строка с общей суммой
+        countGoods = document.querySelector('.counter');
+    let sum = 0;
+    countGoods.textContent = productsCart.length;
+    productsPrice.forEach((Price) => {
+        let price = parseFloat(Price.textContent.slice(1));
+        sum += price;
+    });
+    priceTotal.textContent = sum;
+}
+});
+//изменение страны и валюты
+const country = document.querySelector('#location span');
+country.addEventListener('click', () =>{
+  if(country.textContent == 'РОССИЯ') {
+    country.textContent = 'УКРАИНА';
+  }else{
+    country.textContent = 'РОССИЯ';
+  }
+});
+const currency = document.querySelector('#currency span');
+currency.addEventListener('click', () =>{
+  if(currency.textContent == 'US DOLLAR') {
+    currency.textContent = 'RU РУБЛЬ';
+  }else{
+    currency.textContent = 'US DOLLAR';
+  }
+});
+
+
+
 $(function()
 {
-  "use strict";
-  $('.title_box').click(function()
+  $('.title_box').click(function() //раскрытие списков категорий
   {
      $(this).toggleClass('open');
      $(this).next('.list_link').toggleClass('open');
   });
-  $('#slider_price').slider({
+  $('#slider_price').slider({  //фильтр по цене
     max: 1000, 
     min: 0,
     range: true,
@@ -22,18 +74,20 @@ $(function()
   $('input[name="maxPrice"]').val( 
       $('#slider_price').slider('values', 1) );
 
-  $('#cart, .title_cart').click(function(){
+  $('#cart, .title_cart').click(function(){ //открытие окна корзины и регистрации
     $('#cart_box').toggleClass('open');
   });
   $('#sign').click(function(){
     $('#black_fill').toggleClass('open');
     $('#modal').toggleClass('open');
+    document.body.style.overflow = 'hidden';
   });
   $('#close_mod, #black_fill').click(function(){
     $('#black_fill').toggleClass('open');
     $('#modal').toggleClass('open');
+    document.body.style.overflow = '';
   });
-  $('#search_button').click(function(){
+  $('#search_button').click(function(){ // поиск
     $('#search_panel').toggleClass('open');
   });
   $('#loadmore').click(function(event){
